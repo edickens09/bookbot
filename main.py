3c0640782400838de11eff8a9dbb3831ff631199
edickens09 <edickens09@gmail.com>
2024-01-28 16:30:31 -0500

    added new function to count letters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
         file_contents = f.read()
         #print(file_contents)
         print(count_words(file_contents))
+        print(count_letters(file_contents))
 
 
 def count_words(book):
@@ -11,6 +12,22 @@ def count_words(book):
     for word in count:
         word_count += 1
     return word_count
+
+def count_letters(book):
+    letter_dict = {}
+    lower_string = book.lower()
+    string_split = lower_string.split()
+
+    for word in string_split:
+        for letter in word:
+        
+            if letter in letter_dict:
+                letter_dict[letter] += 1
+
+            else:
+                letter_dict[letter] = 1
+
+    return letter_dict
     
 
 main()
