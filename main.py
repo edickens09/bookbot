41f4723db11de09fc269e7c39ef0f275bf53df64
edickens09 <edickens09@gmail.com>
2024-02-04 14:41:25 -0500

    update new generate_report function

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     with open("books/Frankenstein.txt") as f:
         file_contents = f.read()
         #print(file_contents)
-        print(count_words(file_contents))
-        print(count_letters(file_contents))
+        #print(count_words(file_contents))
+       # print(count_letters(file_contents))
+        print(print_report(file_contents))
 
 
 def count_words(book):
@@ -28,6 +29,25 @@ def count_letters(book):
                 letter_dict[letter] = 1
 
     return letter_dict
-    
 
+def sort_on(dict):
+    return dict["Count"]
+
+def generate_report(book):
+    letter_dict = count_letters(book)
+    remove_dict = []
+    letter_list = []
+
+    for check in letter_dict:
+        if check.isalpha() == False:
+            remove_dict.append(check)
+    for remove in remove_dict:
+        del letter_dict[remove]
+
+    for letter, num in letter_dict.items():
+        letter_list.append({"Letter":letter, "Count":num})
+
+    letter_list.sort(key= sort_on, reverse= True)
+    
+    return letter_list
 main()
